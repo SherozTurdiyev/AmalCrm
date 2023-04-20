399ddd74a4e75a6247890ca264dfebdf162aa25c
Nurillokhon <ubaydullayevnurullo4@gmail.com>
2023-04-20 10:13:08 +0500

    salomat

--- src/components/TeacherCard/TeachCard.jsx
+++ src/components/TeacherCard/TeachCard.jsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import { AiFillMessage, AiFillPhone } from 'react-icons/ai'
+import {BsThreeDots} from 'react-icons/bs'
 
 const TeachCard = () => {
   const mass = [
@@ -8,11 +9,11 @@ const TeachCard = () => {
       name: 'Nurillokh Ubaydullayev',
       role: 'web/mobile developer'
     },
-    {
-      img: 'https://media.licdn.com/dms/image/C4E03AQGVxZLL2U27Lg/profile-displayphoto-shrink_800_800/0/1639589594776?e=2147483647&v=beta&t=KA_sl3r_yFw23_n2FRXVEncjyXdXWNjqxrl9lJqCvcs',
-      name: 'Asadbek Shorahimov',
-      role: 'web developer'
-    },
+    // {
+    //   img: 'https://media.licdn.com/dms/image/C4E03AQGVxZLL2U27Lg/profile-displayphoto-shrink_800_800/0/1639589594776?e=2147483647&v=beta&t=KA_sl3r_yFw23_n2FRXVEncjyXdXWNjqxrl9lJqCvcs',
+    //   name: 'Asadbek Shorahimov',
+    //   role: 'web developer'
+    // },
 
   ]
 
@@ -21,8 +22,8 @@ const TeachCard = () => {
       {
         mass.map((item, index) => {
           return (
-            <div>
-              <div className='bg-slate-200 w-72 px-6 py-6 rounded-lg text-center'>
+            <div key={index}>
+              <div className='bg-slate-200 w-72 px-6 py-6 rounded-lg text-center relative '>
                 <img className='rounded-full w-36 m-auto mb-5' src={item.img} alt="user" />
                 <h5 className='font-bold'>{item.name}</h5>
                 <p className='text-slate-400'>{item.role}</p>
@@ -30,6 +31,7 @@ const TeachCard = () => {
                   <AiFillPhone className='bg-cyan-700 text-white text-4xl rounded-full p-2' />
                   <AiFillMessage className='bg-cyan-700 text-white text-4xl rounded-full p-2' />
                 </div>
+                <BsThreeDots className='fs-4 absolute top-1 right-5' />
               </div>
             </div>
           )

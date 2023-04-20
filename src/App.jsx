f271fe9c312692801bd7bb51d54331d469a794df
JaMeS-18-18 <chitak1818@gmail.com>
2023-04-20 18:28:36 +0500

    2 component done

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,13 @@
 import { useState } from 'react'
-
+import About_Student from './components/About_Student/About_Student'
+import LessonCard from './components/LessonCard/LessonCard'
 function App() {
   const [count, setCount] = useState(0)
 
   return (
     <div className="App">
-      <h1>Hello world!</h1>
+      <About_Student/>
+      {/* <LessonCard/> */}
     </div>
   )
 }
